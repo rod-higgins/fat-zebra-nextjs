0592575b35bf8a3b98fa848c82d215b8fe7d923a
Rod Higgins <rod.higgins@fwc.gov.au>
2025-07-20 23:25:32 +1000

    Release 0.5.7

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -437,7 +437,7 @@ export async function handleHealthCheck(): Promise<NextResponse> {
     successful: true,
     status: 'healthy',
     timestamp: new Date().toISOString(),
-    version: '0.5.6',
+    version: '0.5.7',
   });
 }
 
